1e38e9befbe72ef27ee338c9f0c68dcd17715a1a
radhi75 <https://github.com/radhi75/git.git>
2022-03-17 00:37:03 +0100

    msg

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const app = express();
+
+const workingdays = ["Monday", "Tuesday", "Wednesday", "thursday", "friday"];
+const date = new Date();
+const day = workingdays[date.getDay() - 1];
+
+const hour = date.getHours();
+// const hour = 18;
+const x = (req, res, next) => {
+  if (workingdays.includes(day) && hour >= 9 && hour <= 24) {
+    next();
+  } else {
+    res.render("closed");
+  }
+};
+app.use(x);
+
+app.listen(5000);
+console.log(day);
+
+app.use("/", router);
+console.log("hello world");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public"));
+
+router.get("/", (req, res) => res.render("home"));
+router.get("/services", (req, res) => res.render("services"));
+router.get("/contact", (req, res) => res.render("contact"));
